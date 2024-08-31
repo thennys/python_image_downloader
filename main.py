96621c6b85661f532b4abea08411f6a1cd86667d
thennys <thennyswhyte@gmail.com>
2024-08-30 16:30:39 -1200

    Automatic name generation and image downloading from a url list implemented

--- main.py
+++ main.py
@@ -49,9 +49,38 @@ def download_image(image_url: str, name:str, folder:Optional[str] = None):
         print(f'Error: {e}')
 
 
-if __name__ == '__main__':
-    input_url: str = input("Enter Image url: ")
-    input_name: str = input("Enter Image name to save it: ")
+def download_images_from_list(url_list: list[str], folder: Optional[str] = 'images'):
+    """
+    Downloads images from a list of URLs and saves them with automatically generated names.
+    
+    Args:
+    - url_list (list[str]): A list of image URLs to download.
+    - folder (str, optional): The folder to save the images in. Defaults to 'images'. 
+    
+    """
+    for index, image_url in enumerate(url_list):
+    # Auto-generate a unique name using the index
+        image_name: str = f'image_{index+1}'
+        print(f'downloading image {index+1}/{len(url_list)}..........')
+        try:
+            download_image(image_url, name=image_name, folder=folder)
+        except Exception as e:
+            print(f'Failed to download {image_url}: {e}')
 
-    print('Downloading........')
-    download_image(input_url, name=input_name, folder='images')
+if __name__=='__main__':
+    #LIST OF IMAGE URLs TO DOWNLOAD
+    image_urls = ['https://media.istockphoto.com/id/1415551608/photo/happy-middle-aged-couple-together-on-a-road-trip-caucasian-woman-and-man-having-fun-on.jpg?s=612x612&w=0&k=20&c=LdKfyamobdI069sxBRgpavMNJyQWXsmbWIqPqLhy4uE=',
+                  'https://media.istockphoto.com/id/1176439818/photo/making-a-memory.jpg?s=1024x1024&w=is&k=20&c=Yk_c7POcnawBoIHQEMHu9o2tRsPvvzYwL_XJuqcv5Sg=',
+                  'https://media.istockphoto.com/id/1170543777/photo/enjoying-the-view.jpg?s=1024x1024&w=is&k=20&c=-6zMlhf13ugESHTR2i-hUkgs5KxpRhIOLJ3H-uU-hqs=',
+                  'https://media.istockphoto.com/id/1707927462/photo/road-trip-romance.jpg?s=1024x1024&w=is&k=20&c=iisCxC6I2IwwRF84Tu9_JvDXHmLY5rFMayGx8EzfMuc=',
+                  'https://media.istockphoto.com/id/2161113463/photo/aerial-photography-of-modern-city-skyline.jpg?s=1024x1024&w=is&k=20&c=nl55aVk3g3tHMgiEGGNSMZoRImvmGenkhuxTwEPLk64='
+                  ]
+# if __name__ == '__main__':
+    # input_url: str = input("Enter Image url: ")
+    # input_name: str = input("Enter Image name to save it: ")
+
+    # print('Downloading........')
+    # download_image(input_url, name=input_name, folder='images')
+
+    print('Starting batch download...')
+    download_images_from_list(image_urls)
